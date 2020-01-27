b58e8f48f025e391fc2e719dc433bfe40a6dbd86
Felix Ahlbrecht <felix.ahlbrecht@gmail.com>
2020-01-27 20:03:23 +0100

    delete Runkge_Kutta_4.h

--- source/Runge_Kutta_4.h
+++
@@ -1,5 +0,0 @@
-#include <math.h>
-#include <stdio.h>
-
-void EU(int nSteps, double h, double *t, int nVar, double x[][nVar]);
-void RK4(int nSteps, double h, double *t, int nVar, double x[][nVar]);
